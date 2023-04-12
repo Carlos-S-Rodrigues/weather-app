0a073bcc0157327ebc47e50853fbf067a0988537
Carlos-S-Rodrigues <carlos.schuenck.rodrigues@gmail.com>
2023-04-12 17:23:17 -0300

    feat: Search city

---
+++ weatherapp/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd1d57301c7mshdc0510fca175635p1aebaajsn43219054463b',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
