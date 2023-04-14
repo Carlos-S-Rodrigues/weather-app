1e9ea7f710544c6e019d631a2bc73551074addd1
Carlos-S-Rodrigues <carlos.schuenck.rodrigues@gmail.com>
2023-04-14 11:20:50 -0300

    feat: environment variables

--- weatherapp/src/api.js
+++ weatherapp/src/api.js
@@ -1,8 +1,8 @@
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'd1d57301c7mshdc0510fca175635p1aebaajsn43219054463b',
-    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    'X-RapidAPI-Key': process.env.REACT_APP_GEO_API_KEY,
+    'X-RapidAPI-Host': process.env.REACT_APP_GEO_API_HOST
   }
 }
 
@@ -10,4 +10,4 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
 
-export const WEATHER_API_KEY = 'b92c48d30af336cc0eb2037ec2c60378'
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
